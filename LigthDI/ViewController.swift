2bfaeabefbc7e8993701aed5899e0983c3298762
Artemev Aleksey <bartleby.fla@gmail.com>
2017-07-30 17:11:40 +0300

    Initial Commit

---
+++ LigthDI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LigthDI
+//
+//  Created by Artemev Aleksey on 30.07.17.
+//  Copyright © 2017 Artemev Aleksey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
